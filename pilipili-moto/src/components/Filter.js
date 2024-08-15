f965db337a5bc1482a730b760b519bcfc4c0e3be
Denis Korir <denohrkoryr@gmail.com>
2024-08-15 15:51:38 +0300

    changes made to filter component

--- pilipili-moto/src/components/Filter.js
+++ pilipili-moto/src/components/Filter.js
@@ -8,9 +8,12 @@ function Filter({ setFilter }) {
   return (
     <select onChange={handleFilterChange}>
       <option value="">All Heat Levels</option>
+      <option value="None">None</option>
       <option value="Mild">Mild</option>
+      <option value="Mild to Hot">Mild to Hot</option>
       <option value="Hot">Hot</option>
       <option value="Very Hot">Very Hot</option>
+      <option value="Extremely Hot">Extremely Hot</option>
     </select>
   );
 }
