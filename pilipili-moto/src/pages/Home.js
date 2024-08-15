3d9fd10b9fcb0c8a81f3d8ad0913d5fbf26398d2
Denis Korir <denohrkoryr@gmail.com>
2024-08-15 17:18:49 +0300

    Updated state in PepperList after successful POST

--- pilipili-moto/src/pages/Home.js
+++ pilipili-moto/src/pages/Home.js
@@ -1,9 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PepperList from "../components/PepperList";
 import PepperForm from "../components/PepperForm";
 
 function Home() {
   const [peppers, setPeppers] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/peppers")
+      .then((response) => response.json())
+      .then((data) => setPeppers(data))
+      .catch((error) => console.error("Error fetching peppers:", error));
+  }, []);
 
   const handleAddPepper = (newPepper) => {
     setPeppers([...peppers, newPepper]);
