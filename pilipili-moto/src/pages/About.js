446560f06a3845839ca2c380635d14683b60c33a
Denis Korir <denohrkoryr@gmail.com>
2024-08-15 14:41:20 +0300

    About component added

--- pilipili-moto/src/pages/About.js
+++ pilipili-moto/src/pages/About.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function About() {
-  return <div>About Page</div>;
+  const [shopInfo, setShopInfo] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/shopInfo")
+      .then((response) => response.json())
+      .then((data) => setShopInfo(data));
+  }, []);
+
+  if (!shopInfo) return <div>Loading...</div>;
+
+  return (
+    <div>
+      <h2>About {shopInfo.name}</h2>
+      <p>Opening Time: {shopInfo.openingTime}</p>
+      <p>Closing Time: {shopInfo.closingTime}</p>
+    </div>
+  );
 }
 
 export default About;
