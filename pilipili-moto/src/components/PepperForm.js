179bdbd4bd47605ee0835e605d4e503d1c86ef15
Denis Korir <denohrkoryr@gmail.com>
2024-08-15 16:57:55 +0300

    Added basic structure for PepperForm component

---
+++ pilipili-moto/src/components/PepperForm.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+
+function PepperForm({ onAddPepper }) {
+  const [formData, setFormData] = useState({
+    name: "",
+    type: "",
+    heatLevel: "",
+    price: "",
+    description: "",
+    image: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onAddPepper(formData);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="name"
+        value={formData.name}
+        onChange={handleChange}
+        placeholder="Pepper Name"
+      />
+      <input
+        type="text"
+        name="type"
+        value={formData.type}
+        onChange={handleChange}
+        placeholder="Pepper Type"
+      />
+      <input
+        type="text"
+        name="heatLevel"
+        value={formData.heatLevel}
+        onChange={handleChange}
+        placeholder="Heat Level"
+      />
+      <input
+        type="number"
+        name="price"
+        value={formData.price}
+        onChange={handleChange}
+        placeholder="Price"
+      />
+      <input
+        type="text"
+        name="description"
+        value={formData.description}
+        onChange={handleChange}
+        placeholder="Description"
+      />
+      <input
+        type="text"
+        name="image"
+        value={formData.image}
+        onChange={handleChange}
+        placeholder="Image URL"
+      />
+      <button type="submit">Add Pepper</button>
+    </form>
+  );
+}
+
+export default PepperForm;
