0b33f5a7fe84c360b9b9d640257951890a3f8210
Denis Korir <denohrkoryr@gmail.com>
2024-08-15 17:07:51 +0300

    Set up PepperForm as a controlled component

--- pilipili-moto/src/pages/Home.js
+++ pilipili-moto/src/pages/Home.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import PepperList from "../components/PepperList";
+import PepperForm from "../components/PepperForm";
 
 function Home() {
+  const [peppers, setPeppers] = useState([]);
+
+  const handleAddPepper = (newPepper) => {
+    setPeppers([...peppers, newPepper]);
+  };
+
   return (
     <div>
       <h2>Welcome to Pilipili Moto</h2>
-      <PepperList />
+      <PepperForm onAddPepper={handleAddPepper} />
+      <PepperList peppers={peppers} />
     </div>
   );
 }
