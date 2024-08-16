acbfb1d3947967d5ecc74e8e52441ecd0607ba34
Denis Korir <denohrkoryr@gmail.com>
2024-08-16 17:10:17 +0300

    unnecessary component commented out

--- pilipili-moto/src/components/PepperDetails.js
+++ pilipili-moto/src/components/PepperDetails.js
@@ -1,28 +1,28 @@
-import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+// import React, { useState, useEffect } from "react";
+// import { useParams } from "react-router-dom";
 
-function PepperDetailsPage() {
-  const { id } = useParams();
-  const [pepper, setPepper] = useState(null);
+// function PepperDetailsPage() {
+//   const { id } = useParams();
+//   const [pepper, setPepper] = useState(null);
 
-  useEffect(() => {
-    fetch(`http://localhost:5000/peppers/${id}`)
-      .then((response) => response.json())
-      .then((data) => setPepper(data));
-  }, [id]);
+//   useEffect(() => {
+//     fetch(`http://localhost:5000/peppers/${id}`)
+//       .then((response) => response.json())
+//       .then((data) => setPepper(data));
+//   }, [id]);
 
-  if (!pepper) return <div>Loading...</div>;
+//   if (!pepper) return <div>Loading...</div>;
 
-  return (
-    <div>
-      <h2>{pepper.name}</h2>
-      <p>Type: {pepper.type}</p>
-      <p>Heat Level: {pepper.heatLevel}</p>
-      <p>Price: {pepper.price}</p>
-      <p>Description: {pepper.description}</p>
-      <img src={pepper.image} alt={pepper.name} />
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <h2>{pepper.name}</h2>
+//       <p>Type: {pepper.type}</p>
+//       <p>Heat Level: {pepper.heatLevel}</p>
+//       <p>Price: {pepper.price}</p>
+//       <p>Description: {pepper.description}</p>
+//       <img src={pepper.image} alt={pepper.name} />
+//     </div>
+//   );
+// }
 
-export default PepperDetailsPage;
+// export default PepperDetailsPage;
